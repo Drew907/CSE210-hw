21f8f582745cca90a73dc2821dedbb9727bf5d1a
Drew Stephenson <ste25011@byui.edu>
2026-09-19 20:30:43 -0600

    completed task

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+      Console.WriteLine("Enter you grade in percentage:");
+      string percs =Console.ReadLine();
+      int perc = int.Parse(percs);
+
+    if (perc >= 90)
+        {
+            Console.WriteLine("A");
+        }
+    else if (perc >= 80)
+        {
+            Console.WriteLine("B");
+        }
+    else if (perc >= 70)
+        {
+            Console.WriteLine("C");
+        }
+    else if (perc >= 60)
+        {
+            Console.WriteLine("D");
+        }
+    else {Console.WriteLine("F");
+    }
+
+
+
     }
 }
