319dabf38c10e08e31a906884918ef567d8df74a
Drew Stephenson <ste25011@byui.edu>
2026-09-25 15:43:04 -0600

    done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int gnum = 0;
+        Console.Write("What is the magic number? ");
+        string mNumber =Console.ReadLine();
+        int mnum = int.Parse(mNumber);
+        
+      
+        
+        while(gnum != mnum)
+        {
+            
+        
+            Console.Write("What is your guess?  ");
+            string gNumber =Console.ReadLine();
+        
+
+
+            gnum = int.Parse(gNumber);
+                if (gnum > mnum)
+                    {
+                      Console.WriteLine("Lower");
+                    }
+                else if (gnum < mnum)
+                    {
+                    Console.WriteLine("Higher");
+                    }
+                else
+                    {
+                    Console.WriteLine("You got it!");
+                    }
+        }
+        
     }
-}
+}    
